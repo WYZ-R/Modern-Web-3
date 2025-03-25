bbecb736390e859f4768cfe7e54e0284b5889dc8
wuzhenyu <759799349@qq.com>
2025-03-25 15:38:55 +0800

    feat: Welcome

---
+++ client/tailwind.config.js
@@ -0,0 +1,38 @@
+import forms from "node_modules/@tailwindcss/forms";
+
+export default {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  //   tailwind css的jit模式，可以动态生成工具类 比如可以h-[1px]表示hight:1px;
+  mode: "jit",
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        mf: "990px",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            "-webkit-transform": "translateX(120%)",
+            transform: "translateX(120%)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(0%)",
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease-out",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [forms],
+};
